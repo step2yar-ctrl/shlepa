da0bb2d413ca75793173b222c03eaaa7565b2d79
User <User@gmail.com>
2026-09-07 19:49:08 +0300

    change 123 123
    Signed-off-by: User <User@gmail.com>

--- mine.py
+++ mine.py
@@ -12,3 +12,23 @@ if age > 18:
     print("Вхід дозволено!")
 else:
     print("Вхід заборонено!")
+
+
+#3
+
+import random
+
+number = random.randint(1, 10)
+
+for attempt in range(3):
+    guess = int(input("Вгадайте число від 1 до 10: "))
+
+    if guess == number:
+        print("Ви вгадали!")
+        break
+    elif guess > number:
+        print("Менше")
+    else:
+        print("Більше")
+else:
+    print(f"Ви програли! Загадане число: {number}")
